28f5db42b9e6999b5d92bb46c325b0b2c2fdf227
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-02-15 03:31:55 +0500

    Base setup

--- app/src/main/java/org/abubaker/debugging/MainActivity.kt
+++ app/src/main/java/org/abubaker/debugging/MainActivity.kt
@@ -30,9 +30,46 @@ class MainActivity : AppCompatActivity() {
         // Log the message
         logging()
 
+        // first()
+
+        // This will cause an error
+        division()
+
     }
 
-    fun logging() {
+    fun first() {
+        second()
+        Log.v(TAG, "1")
+    }
+
+    fun second() {
+        third()
+        Log.v(TAG, "2")
+        fourth()
+    }
+
+    fun third() {
+        Log.v(TAG, "3")
+    }
+
+    fun fourth() {
+        Log.v(TAG, "4")
+    }
+
+    private fun division() {
+
+        val numerator = 60
+        var denominator = 4
+
+        repeat(4) {
+            // Log.d(TAG, "$denominator")
+            Log.v(TAG, "${numerator / denominator}")
+            denominator--
+        }
+
+    }
+
+    private fun logging() {
 
         /**
          * 1. e: Error
